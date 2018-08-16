508ca1e812bdbb128ce5c448092622a3f642e9cd
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-16 10:09:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
     client.user.setActivity('YouTube', { type: 'WATCHING' });
+    client.user.setStatus("dnd")
 });
 
 client.on('message', message => {
