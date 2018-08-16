fb200cf49302782950676bac528aef2233448699
kneewoah <42323086+kneewoah@users.noreply.github.com>
2018-08-16 10:03:29 -0400

    Update client.js

--- client.js
+++ client.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    client.user.setActivity('YouTube', { type: 'WATCHING' });
 });
 
 client.on('message', message => {
